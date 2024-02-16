a7afb2e92227e09b082d3dd0c295ed5519775487
Szidor Patrik <patrikp987@gmail.com>
2024-02-16 17:08:15 +0000

    Added relations for models

--- AilrinesAPI/app/Models/City.php
+++ AilrinesAPI/app/Models/City.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class City extends Model
 {
@@ -14,4 +15,13 @@ class City extends Model
         'name',
         'population',
     ];
+
+    protected $casts = [
+        'population' => 'integer'
+    ];
+
+    public function flight(): BelongsTo
+    {
+        return $this->belongsTo(Flight::class);
+    }
 }
--- AilrinesAPI/app/Models/Flight.php
+++ AilrinesAPI/app/Models/Flight.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Flight extends Model
 {
@@ -18,4 +20,20 @@ class Flight extends Model
         'flight_time',
         'huf_per_km',
     ];
+
+    protected $casts = [
+        'distance' => 'integer',
+        'flight_time' => 'integer',
+        'huf_per_km' => 'integer',
+    ];
+
+    public function cities(): HasMany
+    {
+        return $this->hasMany(City::class);
+    }
+
+    public function airline(): HasOne
+    {
+        return $this->hasOne(Airline::class);
+    }
 }
