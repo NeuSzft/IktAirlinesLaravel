a4dde3aa37352f35c15ef749c193efce5cd66776
Törőcsik Ádám <toarexer@gmail.com>
2024-02-26 23:09:33 +0100

    API testing now uses it's own containers.

--- test.sh
+++ test.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
+ARGS='-f compose-api-test.yml --env-file ./AirlinesAPI/.env'
+
 export HOST_UID=$(id -u)
 export HOST_GID=$(id -g)
 
-docker compose -f compose-api-test.yml down
-docker compose -f compose-api-test.yml up -d
-docker compose -f compose-api-test.yml logs -f api-tests
+docker compose $ARGS down
+docker compose $ARGS up -d
+docker compose $ARGS logs -f api-tests
+docker compose $ARGS down
