903670e27c3c832136547714b31482f15ddf3b8e
Törőcsik Ádám <toarexer@gmail.com>
2024-02-29 14:05:38 +0100

    Updated api test compose and shell script

--- test.sh
+++ test.sh
@@ -5,8 +5,7 @@ ARGS='-f compose-api-test.yml --env-file ./AirlinesAPI/.env'
 export HOST_UID=$(id -u)
 export HOST_GID=$(id -g)
 
-docker compose $ARGS down
 docker compose $ARGS up -d
-docker compose $ARGS logs -f api-tests
+docker compose $ARGS wait api-tests
 docker compose $ARGS ps -a
-docker compose $ARGS down
+docker compose $ARGS stop
