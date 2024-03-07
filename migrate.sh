7bcbb56d1dc43e390659bfc6e45c5c6d6f710afb
Törőcsik Ádám <toarexer@gmail.com>
2024-03-07 19:12:08 +0100

    Update migrate.sh

--- migrate.sh
+++ migrate.sh
@@ -2,4 +2,7 @@
 
 ARGS='-f compose.yml --env-file ./docker/conf/.env'
 
+export HOST_UID=$(id -u)
+export HOST_GID=$(id -g)
+
 docker compose $ARGS exec app fish -c 'php artisan migrate:fresh --seed'
